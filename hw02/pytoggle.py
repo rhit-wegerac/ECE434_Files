ff3c817dee9c5d79eb3550835e34e76b089771ac
rhit-wegerac <34016963+rhit-wegerac@users.noreply.github.com>
2021-09-16 02:28:36 -0400

    Last commit for submission

--- hw02/pytoggle.py
+++ hw02/pytoggle.py
@@ -3,18 +3,18 @@ import gpiod
 import time
 
 
-
+#selects which gpio chip we need
 chip1 = gpiod.Chip('gpiochip1')
 
 
-line= chip1.get_lines([28])
-line.request(consumer='blink', type = gpiod.LINE_REQ_DIR_OUT)
+line= chip1.get_lines([28])#sets line to be the GPIO pin
+line.request(consumer='blink', type = gpiod.LINE_REQ_DIR_OUT)#sets the pin as output
 
 
-t = 0.000001
+t = 0.000001 #time for sleep
 
 while True:
-    line.set_values([0])
-    time.sleep(t)
-    line.set_values([1])
-    time.sleep(t)
+    line.set_values([0]) #turns pin off
+    time.sleep(t) #system sleeps
+    line.set_values([1]) #turns pin on
+    time.sleep(t) #system sleeps
