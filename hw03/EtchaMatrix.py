06d1d74c2f58203e57ca217d79f1221124d21208
Andrew Weger <wegerac@rose-hulman.edu>
2021-09-23 10:14:38 +0000

    test

---
+++ hw03/EtchaMatrix.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import smbus
+import gpiod
+import numpy as np
+import sys 
+from curtsies import Input
+
+bus = smbus.SMBus(2)
+matrix = 0x70
+
+matrixGrid = [0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+                
+
+row = [0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000]
+
+width = 7;
+height = 7;
+
+xPos = 0;
+yPos = 0;
+yPosB = 1;
+
+bus.write_i2c_block_data(matrix,0, matrixGrid)
+
+bus.write_byte_data(matrix, 0xFF, 0)
+
+while(1):#game loop
+    with Input(keynames='curses')as input_generator:#detects if there was a key pressed 
+        for e in input_generator:
+            if str(e) == 'KEY_UP': #checks if the up arrow was pressed
+                time.sleep(0.1)#sleepsto give time for the player to lift off the key
+                yPos = yPos + 1
+                yPosB = yPosB * 2
+                if (yPos > height):
+                    yPos = height - 1
+                    yPosB = yPosB / 2
+                if (matrixGrid[xPos] & row[yPos] != row[yPos]):
+                    matrixGrid[xPos] = matrixGrid[xPos] + yPosB
+                    bus.write_i2c_block_data(matrix,0, matrixGrid)
+                    bus.write_byte_data(matrix, 0xFF, 0)
+                
+            if str(e) == "KEY_DOWN":#checks if the down arrow was pressed
+                time.sleep(0.1)#sleeps to give time for the player to lift off the key
+                yPos = yPos - 1
+                yPosB = int(yPosB / 2)
+                if (yPos < 0):
+                    yPos = 0
+                    yPosB = 1 
+                if (matrixGrid[xPos] & row[yPos] != row[yPos]):
+                    matrixGrid[xPos] = matrixGrid[xPos] + yPosB
+                    bus.write_i2c_block_data(matrix,0, matrixGrid)
+                    bus.write_byte_data(matrix, 0xFF, 0)
+            if str(e) == "KEY_LEFT":#checks if the left arrow was pressed
+                time.sleep(0.1)#sleeps to give time for the player to lift off the key
+                xPos = xPos - 2
+                if (xPos < 0):
+                    xPos = 0
+                if (matrixGrid[xPos] & row[yPos] != row[yPos]):
+                    matrixGrid[xPos] = matrixGrid[xPos] + yPosB
+                    bus.write_i2c_block_data(matrix,0, matrixGrid)
+                    bus.write_byte_data(matrix, 0xFF, 0)
+            if str(e) == "KEY_RIGHT":#checks if the right arrow was pressed
+                time.sleep(0.1)# sleeps to give time for the player to lift off the key
+                xPos = xPos + 2
+                if (xPos >= width * 2):
+                    xPos = (width * 2)
+                if (matrixGrid[xPos] & row[yPos] != row[yPos]):
+                    matrixGrid[xPos] = matrixGrid[xPos] + yPosB
+                    bus.write_i2c_block_data(matrix,0, matrixGrid)
+                    bus.write_byte_data(matrix, 0xFF, 0)
+            if str(e) == ' ':#check if the spacebar was pressed
+                time.sleep(0.1)#gives time for the player to lift off the key
+                matrixGrid = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+                bus.write_i2c_block_data(matrix,0, matrixGrid)
+                bus.write_byte_data(matrix, 0xFF, 0)
+            
--- hw03/temp.py
+++ hw03/temp.py
@@ -1,1 +1,55 @@
+#!/usr/bin/env python3
 
+import os
+import time
+import smbus
+import gpiod
+
+# while True:
+#     time.sleep(0.5)
+#     temp1 = os.popen('sudo i2cget -y 2 0x4a 00').read()
+#     temp2 = os.popen('sudo i2cget -y 2 0x48 00').read()
+#     temp1int = (int(temp1,base=16) * 1.8) + 32
+#     temp2int = (int(temp2,base=16) * 1.8) + 32
+#     print("Temp 1: " + str(round(temp1int,2)) + "\N{DEGREE SIGN}" + "F")
+#     print("Temp 2: " + str(round(temp2int,2)) + "\N{DEGREE SIGN}" + "F")
+#     print("*********************")
+
+bus = smbus.SMBus(2)
+address1 = 0x4a
+address2 = 0x48
+
+os.system("sudo i2cset -y 2 0x4a 1 0x00")
+os.system("sudo i2cset -y 2 0x4a 2 0x1D")
+os.system("sudo i2cset -y 2 0x4a 3 0x1E")
+os.system("sudo i2cset -y 2 0x48 1 0x00")
+os.system("sudo i2cset -y 2 0x48 2 0x1D")
+os.system("sudo i2cset -y 2 0x48 3 0x1E")
+
+chip0 = gpiod.Chip('gpiochip0')
+CONSUMER='getset'
+sens1 = chip0.get_lines([30])
+sens2 = chip0.get_lines([31])
+sens1.request(consumer=CONSUMER, type = gpiod.LINE_REQ_DIR_IN)
+sens2.request(consumer=CONSUMER, type = gpiod.LINE_REQ_DIR_IN)
+
+test = bus.read_byte_data(address1,1)
+test1 = bus.read_byte_data(address1,2)
+test2 = bus.read_byte_data(address1,3)
+while True:
+    # temp1 = bus.read_byte_data(address1,0)
+    # temp2 = bus.read_byte_data(address2,0)
+    # print(str(temp1) + " " + str(temp2), end="\r")
+    # time.sleep(0.25)
+    temp1 = sens1.get_values()
+    temp2 = sens2.get_values()
+    if temp1 == [0]:
+        temp1 = (bus.read_byte_data(address1,0) * 1.8) + 32;
+        print("Temperature Sensor 1: " + str(temp1) + "\N{DEGREE SIGN}" + "F")
+        time.sleep(0.5)
+        
+    if temp2 == [0]:
+        temp2 = (bus.read_byte_data(address2,0) * 1.8) + 32;
+        print("Temperature Sensor 2: " + str(temp2) + "\N{DEGREE SIGN}" + "F")
+        time.sleep(0.5)
+    
