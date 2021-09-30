3334e9c3b3f7e8fa1f422d1ce479d043aa89162f
Andrew Weger <wegerac@rose-hulman.edu>
2021-09-30 02:12:35 +0000

    testing mmap

--- hw04/gpiommap.py
+++ hw04/gpiommap.py
@@ -1,1 +1,33 @@
+#!/usr/bin/env python3
+from mmap import mmap
+import time, struct
 
+GPIO1_offset = 0x4804c000
+GPIO1_size = 0x4804cfff-GPIO1_offset
+GPIO_OE = 0x134
+GPIO_SETDATAOUT = 0x194
+GPIO_CLEARDATAOUT = 0x190
+
+p923 = 1<<17
+t = 0.0000000000001
+
+with open("/dev/mem", "r+b" ) as f:
+  mem = mmap(f.fileno(), GPIO1_size, offset=GPIO1_offset)
+
+packed_reg = mem[GPIO_OE:GPIO_OE+4]
+
+reg_status = struct.unpack("<L", packed_reg)[0]
+
+reg_status &= ~(p923)
+
+mem[GPIO_OE:GPIO_OE+4] = struct.pack("<L", reg_status)
+
+try:
+  while(True):
+    mem[GPIO_SETDATAOUT:GPIO_SETDATAOUT+4] = struct.pack("<L", p923)
+    time.sleep(t)
+    mem[GPIO_CLEARDATAOUT:GPIO_CLEARDATAOUT+4] = struct.pack("<L", p923)
+    time.sleep(t)
+
+except KeyboardInterrupt:
+  mem.close()
