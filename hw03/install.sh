9cb1c656c0bc79298283599d1e8f27eaa7860de2
rhit-wegerac <34016963+rhit-wegerac@users.noreply.github.com>
2021-10-02 12:02:53 -0400

    Update install.sh

--- hw03/install.sh
+++ hw03/install.sh
@@ -1,2 +1,11 @@
-pip3 install numpy
-pip3 install curtsies
+# eQEP 1
+config-pin P8_33 qep
+config-pin P8_35 qep
+
+# eQEP 2
+config-pin P8_11 qep    # These have a conflict on the Green Wireless
+config-pin P8_12 qep
+
+config-pin P8_41 qep
+config-pin P8_42 qep
+
